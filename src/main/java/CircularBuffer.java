b6277571f6ab303a48c8e42b747135b459cd1cc4
Supachai Uthawisan <supachaiuth@mathregit.group>
2020-08-03 15:55:26 +0700

    CircleBuffer

---
+++ src/main/java/CircularBuffer.java
@@ -0,0 +1,24 @@
+public class CircularBuffer {
+
+    private int buffersize = 10;
+    private String[] buffer = new String[10];
+    private int readPointer;
+    private int writePointer;
+
+    public boolean isEmpty() {
+        return buffersize == 10;
+    }
+
+    public void writeData(String input) {
+        buffersize--;
+        this.buffer[writePointer++] = input;
+    }
+
+    public boolean isFull() {
+        return buffersize == 0;
+    }
+
+    public String readData() {
+        return this.buffer[readPointer++];
+    }
+}
